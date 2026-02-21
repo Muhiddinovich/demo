994464061c5cefc030ed47fcf70f6769ce7ea415
NestOne Office <mullagoziev@gmail.com>
2026-02-21 06:04:25 -0500

    Method reference with generics 2. Using method reference to Comparator

---
+++ study/src/chapter14_lambda_expressions/method_reference/with_generics2/MyClass.java
@@ -0,0 +1,13 @@
+package chapter14_lambda_expressions.method_reference.with_generics2;
+
+public class MyClass {
+	private int val;
+
+	public MyClass(int v) {
+		val = v;
+	}
+
+	int getVal() {
+		return val;
+	}
+}
