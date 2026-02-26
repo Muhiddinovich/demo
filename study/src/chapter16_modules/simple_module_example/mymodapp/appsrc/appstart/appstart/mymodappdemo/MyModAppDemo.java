72bde9ad1122c0b75df7b43745f74e8a90fa766a
NestOne Office <mullagoziev@gmail.com>
2026-02-26 04:47:43 -0500

    Modules - basic

---
+++ study/src/chapter16_modules/simple_module_example/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,14 @@
+package chapter16_modules.simple_module_example.mymodapp.appsrc.appstart.appstart.mymodappdemo;
+
+import chapter16_modules.simple_module_example.mymodapp.appsrc.appfuncs.appfuncs.simplfuncs.SimpleMathFuncs;
+
+public class MyModAppDemo {
+	public static void main(String[] args) {
+		if (SimpleMathFuncs.isFactor(2, 10))
+			System.out.println("2 is a factor of 10");
+
+		System.out.println("Smallest factor common to both 35 and 105 is " + SimpleMathFuncs.lcf(35, 105));
+
+		System.out.println("Largest factor common to both 35 and 105 is " + SimpleMathFuncs.gcf(35, 105));
+	}
+}
