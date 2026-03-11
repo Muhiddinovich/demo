00b130e11e04b6149a6fc123fe6d7d2f8ae703bd
Fahriddin <mullagoziev@gmail.com>
2026-03-11 11:04:26 -0400

    Records - basics

---
+++ study/src/chapter17_added_features/records/basics/Employee.java
@@ -0,0 +1,5 @@
+package chapter17_added_features.records.basics;
+
+public record Employee(String name, int idNum) {
+
+}
---
+++ study/src/chapter17_added_features/records/basics/RecordDemo.java
@@ -0,0 +1,15 @@
+package chapter17_added_features.records.basics;
+
+public class RecordDemo {
+	public static void main(String[] args) {
+		Employee [] empList =new Employee[4];
+		
+		empList[0]=new Employee("Doe, John", 1047);
+		empList[1]=new Employee("Jones, Robert", 1048);
+		empList[2]=new Employee("Smith, Rachel", 1049);
+		empList[3]=new Employee("Martin, Dave", 1050);
+		
+		for(Employee e: empList)
+			System.out.println("The employee ID for "+e.name()+" is "+e.idNum());
+	}
+}
