5cb7332ce42ba7b2612e271c2f7038f146329e4d
Fahriddin <mullagoziev@gmail.com>
2026-03-17 12:38:16 -0400

    Traditional switch and use of list constants

---
+++ study/src/chapter17_added_features/switch_enhancements/list_of_case_constants/LiseOfCase.java
@@ -0,0 +1,26 @@
+package chapter17_added_features.switch_enhancements.list_of_case_constants;
+
+public class LiseOfCase {
+
+	public static void main(String[] args) {
+		int priorityLevel;
+		int eventCode = 6010;
+
+		switch (eventCode) {
+		case 1000,1205,8900:
+			priorityLevel = 1;
+			break;
+		case 2000,6010,9128:
+			priorityLevel = 2;
+			break;
+		case 1002,7023,9300:
+			priorityLevel = 3;
+			break;
+		default:
+			priorityLevel = 0;
+		}
+
+		System.out.println("Priority level of event code " + eventCode + " is " + priorityLevel);
+	}
+
+}
