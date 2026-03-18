70b4d2f5e8b3865d3e2333a2d23b69192e2691f2
Fahriddin <mullagoziev@gmail.com>
2026-03-18 23:00:51 +0500

    Text blocks

---
+++ study/src/chapter17_added_features/text_blocks/fundamentals/Demo.java
@@ -0,0 +1,14 @@
+package chapter17_added_features.text_blocks.fundamentals;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String str="""
+				Text blocks make 
+				multiple lines easy
+				""";
+		
+		System.out.println(str);
+	}
+
+}
