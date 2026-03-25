6e0649ea39d5d3dde6006da6fa7c4cb38e955a87
NestOne Office <mullagoziev@gmail.com>
2026-03-25 06:53:03 -0400

    CharAt()

---
+++ study/src/chapter18_string_handling/character_extraction/charAt/Demo.java
@@ -0,0 +1,9 @@
+package chapter18_string_handling.character_extraction.charAt;
+
+public class Demo {
+	public static void main(String[] args) {
+		char ch;
+		ch="abc".charAt(2);
+		System.out.println(ch);
+	}
+}
