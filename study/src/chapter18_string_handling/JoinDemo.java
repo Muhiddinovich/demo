4f90dec463528706911d2f108f2bdf081c09e882
Fahriddin <mullagoziev@gmail.com>
2026-03-26 14:09:01 -0400

    Join

---
+++ study/src/chapter18_string_handling/JoinDemo.java
@@ -0,0 +1,9 @@
+package chapter18_string_handling;
+
+public class JoinDemo {
+
+	public static void main(String[] args) {
+		System.out.println(String.join("##", "Alpha", "Beta", "Gamma"));
+	}
+
+}
