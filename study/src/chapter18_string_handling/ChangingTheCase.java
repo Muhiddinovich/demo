429cd6f4d333560d356f53ca83152ef1d62125d7
Fahriddin <mullagoziev@gmail.com>
2026-03-26 13:57:22 -0400

    Changin the cases

---
+++ study/src/chapter18_string_handling/ChangingTheCase.java
@@ -0,0 +1,11 @@
+package chapter18_string_handling;
+
+public class ChangingTheCase {
+
+	public static void main(String[] args) {
+		String s = "This is a TEST";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+	}
+
+}
