63a315fe7e994a71aaab3b8e79aa060154f34012
Fahriddin <mullagoziev@gmail.com>
2026-03-26 11:00:58 -0400

    Searching strings

---
+++ study/src/chapter18_string_handling/searching_string/SearchingString.java
@@ -0,0 +1,19 @@
+package chapter18_string_handling.searching_string;
+
+public class SearchingString {
+
+	public static void main(String[] args) {
+		String s = "Now is the time for all good men " + "to come to the aid of their country.";
+		System.out.println(s);
+		System.out.println(s.indexOf('t'));
+		System.out.println(s.lastIndexOf('t'));
+		System.out.println(s.indexOf("the"));
+		System.out.println(s.lastIndexOf("the"));
+		System.out.println(s.indexOf('t', 10));
+		System.out.println(s.lastIndexOf('t', 60));
+		System.out.println(s.indexOf("the", 10));
+		System.out.println(s.lastIndexOf("the", 60));
+		System.out.println(s.indexOf("the", 3, 47));
+	}
+
+}
