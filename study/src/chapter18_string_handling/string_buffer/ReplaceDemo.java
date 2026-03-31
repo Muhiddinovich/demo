1754e30f33b9673cf2956b61496e6e0f1e8e97ff
Fahriddin <mullagoziev@gmail.com>
2026-03-31 11:40:07 -0400

    replace

--- study/src/chapter18_string_handling/string_buffer/ReplaceDemo.java
+++ study/src/chapter18_string_handling/string_buffer/ReplaceDemo.java
@@ -3,8 +3,9 @@ package chapter18_string_handling.string_buffer;
 public class ReplaceDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringBuffer buffer = new StringBuffer("This is a test");
+		buffer.replace(5, 7, "was");
+		System.out.println(buffer);
 	}
 
 }
