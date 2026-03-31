0b8b009096a48c9120aa73607d4ab9869ff02691
Fahriddin <mullagoziev@gmail.com>
2026-03-31 11:16:40 -0400

    reverse

---
+++ study/src/chapter18_string_handling/string_buffer/ReverseDemo.java
@@ -0,0 +1,11 @@
+package chapter18_string_handling.string_buffer;
+
+public class ReverseDemo {
+
+	public static void main(String[] args) {
+		StringBuffer buffer = new StringBuffer("Heeeyee");
+		buffer.reverse();
+		System.out.println(buffer);
+	}
+
+}
