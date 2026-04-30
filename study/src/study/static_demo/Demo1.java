389f8f9ad8624fc4cd78e112f943ddbba35e9a83
NestOne Office <mullagoziev@gmail.com>
2026-04-29 21:38:25 -0400

    Static Demo

---
+++ study/src/study/static_demo/Demo1.java
@@ -0,0 +1,9 @@
+package study.static_demo;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		StaticDemo.callme();
+		System.out.println("b= " + StaticDemo.b);
+	}
+
+}
---
+++ study/src/study/static_demo/StaticDemo.java
@@ -0,0 +1,11 @@
+package study.static_demo;
+
+public class StaticDemo {
+	static int a = 56;
+	static int b = 78;
+
+	static void callme() {
+		System.out.println("a= " + a);
+	}
+
+}
