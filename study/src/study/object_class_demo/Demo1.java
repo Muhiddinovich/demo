01409119906902e5d6cad85c106c333eb034760b
Fahriddin <mullagoziev@gmail.com>
2026-05-12 10:23:34 -0400

    Object class

---
+++ study/src/study/object_class_demo/Demo1.java
@@ -0,0 +1,13 @@
+package study.object_class_demo;
+
+public class Demo1 {
+	public static void main(String[] arg) {
+		Point point1 = new Point(5, -5);
+		Point point2 = point1;
+		Point point3 = new Point(5, -5);
+		Point point4 = new Point(5, 5);
+		System.out.println(point1.equals(point2));
+		System.out.println(point1.equals(point3));
+		System.out.println(point1.equals(point4));
+	}
+}
---
+++ study/src/study/object_class_demo/Point.java
@@ -0,0 +1,24 @@
+package study.object_class_demo;
+
+public class Point {
+	int x;
+	int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		
+		return this.x==other.x &&this.y==other.y;
+	}
+}
